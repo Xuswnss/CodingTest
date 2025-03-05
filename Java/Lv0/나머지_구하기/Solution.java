aa04da29f16b1d44d76fe96253451b3ffb328ab1
Xuswnss <cooconut0226@gmail.com>
2025-03-05 17:54:57 +0900

    feat(3/5): 1. 나머지 구하기

---
+++ Java/Lv0/나머지_구하기/Solution.java
@@ -0,0 +1,19 @@
+package Java.Lv0.나머지_구하기;
+
+/*
+* 정수 num1, num2가 매개변수로 주어질 때,
+* num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+
+ */
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 % num2;
+        return answer;
+    }// end solution
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(3, 3));
+    }
+}// end Solution
